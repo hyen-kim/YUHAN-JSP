0335597675ac0d3a4b377d58412ca716cb9711f2
김혜인 <k_hn_n@naver.com>
2020-02-03 20:17:44 +0900

    section01 : doGet, doPost 방식

---
+++ eclipse/section01-Servlet/src/com/servlet/ex/PostMethod.java
@@ -0,0 +1,55 @@
+package com.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostMethod
+ */
+@WebServlet("/PostMethod")
+public class PostMethod extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostMethod() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>doPost</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
+	}
+
+}
