12199c55ad752b7dcb6ceafaf1bc85cdfee0c96e
김혜인 <k_hn_n@naver.com>
2020-02-03 20:39:28 +0900

    section01 : init, destroy

---
+++ alone-study/section01-Servlet/src/com/servlet/ex/LifeCycleEx.java
@@ -0,0 +1,54 @@
+package com.servlet.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleEx
+ */
+@WebServlet("/LifeCycleEx")
+public class LifeCycleEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+	}
+	
+	
+	@Override
+	public void destroy() {
+		// 자원을 해제할때 마지막으로 한번 실행
+		System.out.println("destroy");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		// 최초 요청이 들어왔을 때 딱 한번 실행
+		System.out.println("init");
+	}
+
+}
