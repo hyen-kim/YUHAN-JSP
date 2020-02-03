538e6e58789a391674b35b7b16917c4edba978ec
김혜인 <k_hn_n@naver.com>
2020-02-03 20:15:55 +0900

    testServers 수정

--- eclipse/testServers/src/com/javalec/ex/Helloworld.java
+++ eclipse/testServers/src/com/javalec/ex/Helloworld.java
@@ -1,6 +1,8 @@
 package com.javalec.ex;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,8 +30,25 @@ public class Helloworld extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     
+    // 					   요청처리객체 				 	  응답처리객체 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello world");
+		System.out.println("Hello world");	// Console 출력
+		
+		// html 코드로 응답을 하겠다는 의미 		, 	한글을 안깨지게 하기위해
+		response.setContentType("text/html; charser=euc-kr");
+		
+		PrintWriter writer = response.getWriter();
+		
+		// html 코드 
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>HelloWorld</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
 	}
 
 	/**
